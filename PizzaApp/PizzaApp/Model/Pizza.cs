c07c72a0f79c4afc12f0c1f2755fc0d023d9cf80
moudAB <81109037+moudAB@users.noreply.github.com>
2021-08-25 15:28:22 -0400

    création d'une application liste de pizzas qui permet de trier cette liste selon le prix, nom et la pizza favori

---
+++ PizzaApp/PizzaApp/Model/Pizza.cs
@@ -0,0 +1,19 @@
+﻿using PizzaApp.extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaApp.Model
+{
+    class Pizza
+    {
+        public string nom { set; get; }
+        public string imageUrl { set; get; }
+        public int prix { set; get; }
+        public string[] ingredients { set; get; }
+        public string PrixEuros { get { return prix + "€"; } }
+        public string IngredientsStr { get { return string.Join(", ",ingredients); } }
+        public string titre { get { return nom.PremiereLettreMajuscule(); } }
+
+    }
+}
